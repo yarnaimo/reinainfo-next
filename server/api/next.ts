77fec10bb4e960de1bd0e2f63c23230e389eb638
yamaimo <yarnaimo@gmail.com>
2019-11-23 02:14:29 +0900

    process.env.FUNCTION_NAME === 'next'

--- server/api/next.ts
+++ server/api/next.ts
@@ -2,14 +2,17 @@ import { Request, Response } from 'express'
 import next from 'next'
 import { env } from '../../src/env'
 
-const nextServer = next({
-    dev: env.isDev(),
-    dir: 'src',
-})
+const nextServer =
+    process.env.FUNCTION_NAME === 'next'
+        ? next({
+              dev: env.isDev(),
+              dir: 'src',
+          })
+        : null
 
-const handle = nextServer.getRequestHandler()
+const handle = nextServer?.getRequestHandler()
 
 export const _next = async (req: Request, res: Response) => {
     console.log('File: ' + req.originalUrl) // log the page.js file that is being requested
-    return nextServer.prepare().then(() => handle(req, res))
+    return nextServer!.prepare().then(() => handle!(req, res))
 }
