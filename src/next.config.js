aadb3b9ccfbb96be4eeb9315fc54f82ab96ba3da
yamaimo <yarnaimo@gmail.com>
2019-11-26 21:52:52 +0900

    close #11 registerSW

--- src/next.config.js
+++ src/next.config.js
@@ -1,4 +1,4 @@
-const { resolve } = require('path')
+const { resolve, join } = require('path')
 const { readFileSync } = require('fs')
 const webpack = require('webpack')
 
@@ -14,41 +14,53 @@ const firebaserc = JSON.parse(
 const projectId = firebaserc.projects.default
 const dev = process.env.NODE_ENV !== 'production'
 
-module.exports = withFonts(
-    withCss(
-        withSass(
-            withPWA({
-                assetPath: dev ? '' : `https://static-${projectId}.web.app`,
-                distDir: '.next',
-                pwa: {
-                    dest: 'public',
-                },
-                experimental: {
-                    publicDirectory: true,
-                },
-                sassLoaderOptions: {
-                    includePaths: [resolve(__dirname, '../node_modules')],
-                },
-                /**
-                 * @param {webpack.Configuration} [config]
-                 */
-                webpack: config => {
-                    config.plugins = config.plugins || []
+const registerSW = config => {
+    registerScript = join(__dirname, 'register.js')
+    console.log(
+        `> [PWA] auto register service worker with: ${resolve(registerScript)}`,
+    )
 
-                    config.plugins.push(
-                        new webpack.IgnorePlugin({
-                            resourceRegExp: /(?:^|\/)firebase-admin$/,
-                            contextRegExp: /.*/,
-                        }),
-                        // new webpack.IgnorePlugin({
-                        //     resourceRegExp: /^got$/,
-                        //     contextRegExp: /.*/,
-                        // }),
-                        // new BundleAnalyzerPlugin(),
-                    )
-                    return config
-                },
+    const entry = config.entry
+    config.entry = async () =>
+        entry().then(entries => {
+            if (
+                entries['main.js'] &&
+                !entries['main.js'].includes(registerScript)
+            ) {
+                entries['main.js'].unshift(registerScript)
+            }
+            return entries
+        })
+}
+
+const config = {
+    assetPath: dev ? '' : `https://static-${projectId}.web.app`,
+    distDir: '.next',
+    register: false,
+    pwa: {
+        dest: 'public',
+    },
+    experimental: {
+        publicDirectory: true,
+    },
+    sassLoaderOptions: {
+        includePaths: [resolve(__dirname, '../node_modules')],
+    },
+    /**
+     * @param {webpack.Configuration} [config]
+     */
+    webpack: config => {
+        registerSW(config)
+
+        config.plugins = config.plugins || []
+        config.plugins.push(
+            new webpack.IgnorePlugin({
+                resourceRegExp: /(?:^|\/)firebase-admin$/,
+                contextRegExp: /.*/,
             }),
-        ),
-    ),
-)
+        )
+        return config
+    },
+}
+
+module.exports = withFonts(withCss(withSass(withPWA(config))))
