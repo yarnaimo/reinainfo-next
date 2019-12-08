d0d28c6d728913ab74987e01ea86874751905977
yamaimo <yarnaimo@gmail.com>
2019-12-08 23:41:03 +0900

    @yarnaimo/next-config

--- src/next.config.js
+++ src/next.config.js
@@ -1,15 +1,6 @@
-const { resolve, join } = require('path')
+const { resolve } = require('path')
 const { readFileSync } = require('fs')
-const webpack = require('webpack')
-
-const withSass = require('@zeit/next-sass')
-const withCss = require('@zeit/next-css')
-const withPWA = require('next-pwa')
-const withFonts = require('next-fonts')
-// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANALYZE === 'true',
-})
+const withYarnaimoConfig = require('@yarnaimo/next-config')
 
 const firebaserc = JSON.parse(
     readFileSync(resolve(__dirname, '../.firebaserc'), 'utf8'),
@@ -17,30 +8,8 @@ const firebaserc = JSON.parse(
 const projectId = firebaserc.projects.default
 const dev = process.env.NODE_ENV !== 'production'
 
-const registerSW = config => {
-    if (dev) {
-        return
-    }
-    const registerScript = join(__dirname, 'register.js')
-    console.log(
-        `> [PWA] auto register service worker with: ${resolve(registerScript)}`,
-    )
-
-    const entry = config.entry
-    config.entry = async () =>
-        entry().then(entries => {
-            if (
-                entries['main.js'] &&
-                !entries['main.js'].includes(registerScript)
-            ) {
-                entries['main.js'].unshift(registerScript)
-            }
-            return entries
-        })
-}
-
 const nextConfig = {
-    assetPath: dev ? '' : `https://static-${projectId}.web.app`,
+    assetPrefix: dev ? '' : `https://static-${projectId}.web.app`,
     distDir: '.next',
     pwa: {
         dest: 'public',
@@ -52,23 +21,6 @@ const nextConfig = {
     sassLoaderOptions: {
         includePaths: [resolve(__dirname, '../node_modules')],
     },
-    /**
-     * @param {webpack.Configuration} [config]
-     */
-    webpack: config => {
-        registerSW(config)
-
-        config.plugins = config.plugins || []
-        config.plugins.push(
-            new webpack.IgnorePlugin({
-                resourceRegExp: /(?:^|\/)firebase-admin$/,
-                contextRegExp: /.*/,
-            }),
-        )
-        return config
-    },
 }
 
-module.exports = withBundleAnalyzer(
-    withFonts(withCss(withSass(withPWA(nextConfig)))),
-)
+module.exports = withYarnaimoConfig(nextConfig)
--- src/sw-skip-waiting.js
+++
@@ -1,5 +0,0 @@
-self.addEventListener('message', event => {
-    if (event.data && event.data.type === 'SKIP_WAITING') {
-        skipWaiting()
-    }
-})
