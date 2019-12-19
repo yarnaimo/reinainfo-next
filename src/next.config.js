19a0d369e5f29a9d45ef1c71dd904712fd5d2e50
yamaimo <yarnaimo@gmail.com>
2019-12-19 18:29:13 +0900

    remove append-skip-waiting

--- src/next.config.js
+++ src/next.config.js
@@ -1,15 +1,7 @@
 const { resolve } = require('path')
-const { readFileSync } = require('fs')
 const withYarnaimoConfig = require('@yarnaimo/next-config')
 
-const firebaserc = JSON.parse(
-    readFileSync(resolve(__dirname, '../.firebaserc'), 'utf8'),
-)
-const projectId = firebaserc.projects.default
-const dev = process.env.NODE_ENV !== 'production'
-
 const nextConfig = {
-    // assetPrefix: dev ? '' : `https://static-${projectId}.web.app`,
     distDir: '.next',
     pwa: {
         dest: 'public',
