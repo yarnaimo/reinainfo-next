a15c7913e4d3a1b732edd50f9ca572f4bce78ac6
yamaimo <yarnaimo@gmail.com>
2020-09-24 14:44:32 +0900

    add eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+/**
+ * @type {import('eslint').Linter.Config}
+ */
+const config = {
+  extends: ['@yarnaimo'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    // 'no-restricted-imports': [
+    //   'error',
+    //   {
+    //     paths: ['firebase', 'remeda', 'runtypes', 'dayjs'],
+    //     patterns: ['**/__mocks__/**'],
+    //   },
+    // ],
+  },
+}
+
+module.exports = config
