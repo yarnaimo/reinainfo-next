471fe033fa0b87a4f6e4502dd7d0b95cc1946459
yamaimo <yarnaimo@gmail.com>
2020-09-24 18:17:25 +0900

    update next.js config

--- next.config.js
+++ next.config.js
@@ -1,20 +1,34 @@
 const { resolve } = require('path')
-const withYarnaimoConfig = require('@yarnaimo/next-config')
+const webpack = require('webpack')
+const withPWA = require('next-pwa')
+const withFonts = require('next-fonts')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 
 const nextConfig = {
   distDir: '.next',
   pwa: {
+    disable: process.env.NODE_ENV === 'development',
     dest: 'public',
-    register: false,
   },
-  experimental: {
-    publicDirectory: true,
+  sassOptions: {
+    includePaths: [resolve(__dirname, './node_modules')],
   },
-  sassLoaderOptions: {
-    sassOptions: {
-      includePaths: [resolve(__dirname, './node_modules')],
-    },
+
+  /**
+   * @param {webpack.Configuration} [config]
+   */
+  webpack(config) {
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /(?:^|\/)firebase-admin$/,
+        contextRegExp: /.*/,
+      }),
+    )
+
+    return config
   },
 }
 
-module.exports = withYarnaimoConfig(nextConfig)
+module.exports = withPWA(withFonts(withBundleAnalyzer(nextConfig)))
